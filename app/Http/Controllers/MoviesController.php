5c6a31ef3308bd0ca28e8d5712f2891f29a6a615
brankaDakic10 <branka.dakic@yahoo.com>
2018-06-10 10:13:29 +0200

    4.Task -Added validation

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Movie;
+use Validator;
 class MoviesController extends Controller
 {
     public function index()
@@ -21,6 +22,17 @@ class MoviesController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:movies',
+            'director' => 'required',
+            'duration' => 'required|numeric|between:1,500',
+            'releaseDate' => 'required|unique:movies',
+            'imageUrl' => 'url'
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse($validator->errors(), 406);
+        }
         
         $movie=new Movie();
         
@@ -35,6 +47,17 @@ class MoviesController extends Controller
     }
     public function update(Request $request, $id)
     {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|unique:movies',
+            'director' => 'required',
+            'duration' => 'required|numeric|between:1,500',
+            'releaseDate' => 'required|unique:movies',
+            'imageUrl' => 'url'
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse($validator->errors(), 406);
+        }
        $movie=Movie::find($id);
        $movie->title=$request->input('title');
        $movie->director=$request->input('director');
